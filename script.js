127945ee150b6790bd06df5b16e7dccf56d4603a
NuperSoaveOffical <120759297+Nupersoavee@users.noreply.github.com>
2023-08-11 01:10:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 document.getElementById("getWeatherButton").addEventListener("click", function() {
     // You would need to replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
-    const apiKey = "1f66dd79358380ec43a3317593f2c586";
+    const apiKey = "YOUR_API_KEY";
     
-    // Retrieve the user's location (latitude and longitude)
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success, error);
+    const cityInput = document.getElementById("cityInput").value;
+
+    // If the user entered a city name, use it to get the weather
+    if (cityInput) {
+        getWeatherByCity(cityInput, apiKey);
     } else {
-        error();
+        // If no city name entered, try to get weather based on location
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error);
+        } else {
+            error();
+        }
     }
 
     function success(position) {
@@ -39,3 +46,26 @@ document.getElementById("getWeatherButton").addEventListener("click", function()
         document.getElementById("weatherResult").innerHTML = "Location not available. Please enter a city name.";
     }
 });
+
+function getWeatherByCity(city, apiKey) {
+    // Make an API request to OpenWeatherMap using the provided city name
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+        // Display weather information
+        const tempC = Math.round(data.main.temp - 273.15);
+        const tempF = Math.round((tempC * 9/5) + 32);
+        const windMph = Math.round(data.wind.speed * 2.23694);
+
+        document.getElementById("weatherResult").innerHTML = `
+            <p>Temperature: ${tempC}°C / ${tempF}°F</p>
+            <p>Precipitation: N/A</p>
+            <p>Wind Speed: ${windMph} mph</p>
+            <p>Current Conditions: ${data.weather[0].description}</p>
+        `;
+    })
+    .catch(error => {
+        console.error("Error fetching weather data:", error);
+        document.getElementById("weatherResult").innerHTML = "Error fetching weather data.";
+    });
+}
